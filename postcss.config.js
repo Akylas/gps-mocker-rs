f33cc25819b80752bde980de9f78edb51183e8f5
Martin Guillon <martin.guillon@akylas.fr>
2022-03-14 13:59:14 +0100

    chore: configs

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,5 @@
 const cssnano = require('cssnano');
 const mode = process.env.NODE_ENV;
-const dev = mode === 'development';
 const production = mode === 'production';
 
 const config = {
--- vite.config.js
+++ vite.config.js
@@ -5,46 +5,52 @@ const ignoreWarnings = new Set(['a11y-no-onchange', 'a11y-label-has-associated-c
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const config = require('./package.json');
-export default defineConfig(({ command, mode }) => ({
-    root: './src',
-    base: './', // use relative paths
-    publicDir: '../public',
-    clearScreen: false,
-    server: {
-        port: 3011,
-        strictPort: true
-    },
-    resolve: {
-        alias:
-            mode !== 'production'
-                ? {
+export default defineConfig(({ command, mode }) => {
+    const production = mode === 'production';
+    console.log('mode', mode);
+    return {
+        root: './src',
+        base: './', // use relative paths
+        publicDir: '../public',
+        clearScreen: false,
+        server: {
+            port: 3011,
+            strictPort: true
+        },
+        resolve: {
+            alias: production
+                ? {}
+                : {
                       './carbon.scss': 'carbon-components-svelte/css/g90.css'
                   }
-                : {}
-    },
-    build: {
-        outDir: '../build',
-        emptyOutDir: true,
-        minify: mode === 'production',
-        sourcemap: mode !== 'production',
-        target: 'modules'
-    },
-    plugins: [
-        svelte({
-            onwarn(warning, defaultHandler) {
-                // don't warn on <marquee> elements, cos they're cool
-                if (ignoreWarnings.has(warning.code)) return;
+        },
+        optimizeDeps:{
+            // include: ['geo-three']
+        },
+        build: {
+            outDir: '../build',
+            emptyOutDir: true,
+            minify: true,
+            sourcemap: false,
+            target: 'modules'
+        },
+        plugins: [
+            svelte({
+                onwarn(warning, defaultHandler) {
+                    // don't warn on <marquee> elements, cos they're cool
+                    if (ignoreWarnings.has(warning.code)) return;
 
-                // handle all other warnings normally
-                defaultHandler(warning);
-            },
-            preprocess: sveltePreprocess()
-        })
-    ],
-    define: {
-        REPO_URL: `"${config.homepage}"`,
-        FORCE_MOBILE: false,
-        EXTERNAL_APP: false,
-        PRODUCTION: mode === 'production'
-    }
-}));
+                    // handle all other warnings normally
+                    defaultHandler(warning);
+                },
+                preprocess: sveltePreprocess()
+            })
+        ],
+        define: {
+            REPO_URL: `"${config.homepage}"`,
+            FORCE_MOBILE: false,
+            EXTERNAL_APP: false,
+            PRODUCTION: production
+        }
+    };
+});
