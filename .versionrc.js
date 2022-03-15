95265ad1c5d4b8150522482eb9a37ba256d8448c
Martin Guillon <martin.guillon@akylas.fr>
2022-03-15 15:29:50 +0100

    chore: release script to update version

---
+++ .versionrc.js
@@ -0,0 +1,10 @@
+console.log('test')
+const tracker = {
+    filename: './src-tauri/Cargo.toml',
+    updater: require('./tauriVersioner')
+  }
+  
+  module.exports = {
+    bumpFiles: [tracker],
+    packageFiles: [tracker]
+  }
